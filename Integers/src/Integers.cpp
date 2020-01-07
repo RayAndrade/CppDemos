51464c5d47947f56cd920726c0dee1b3f607d1a9
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-06 23:57:42 -0800

    C++Tutorial for Beginners 9 - Integer Variable Types

--- Integers/src/Integers.cpp
+++ Integers/src/Integers.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : Integers.cpp
 // Author      : C++Tutorial for Beginners 9 - Integer Variable Types
-// Version     : https://www.youtube.com/watch?v=oVo0NVGUsU0&list=PLmpc3xvYSk4wDCP5zjt2QQXe8-JGHa4Kt&index=9
+// Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, ANSI-style
 //============================================================================
@@ -18,17 +18,16 @@ int main() {
 	int value2 = 10000000000;
 	long value3 = 10000000000;
 
-	cout << value2 << endl;
-	cout << value3 << endl;
+	cout << "value2" << value2 << endl;
+	cout << "value3" << value3 << endl;
 
-	cout << INT_MAX << endl;
-	cout << value3 - INT_MAX << endl;
-
-	cout << INT_MIN << endl;
+	cout << "Miximun int value: " << INT_MAX << endl;
+	cout << "Minimum int value: " << INT_MIN << endl;
 
 	cout << "Size of int " << sizeof(int) << endl;
 	cout << "Size of short int " << sizeof(short int) << endl;
 	cout << "Size of unsigned int " << sizeof(unsigned int) << endl;
 
 	return 0;
+	// study : https://www.tutorialspoint.com/c_standard_library/limits_h.htm
 }
