e9959ec97ece630c8be0e94a7d4c6a8902f00bd3
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-11 14:34:15 -0800

    C++Tutorial for Beginners 17 - The Do-While Loops

---
+++ Do-While/src/do-while.cpp
@@ -0,0 +1,29 @@
+//============================================================================
+// Name        : Do-While.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++Tutorial for Beginners 17 - The Do-While Loops
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	const string password = "hello";
+
+	string input;
+
+	do {
+		cout << "Enter your password > " << flush;
+		cin >> input;
+		if(input != password){
+			cout << "Access denied" << endl;
+		}
+
+	} while (input != password);
+
+	cout << "Password accepted" << endl;
+
+	return 0;
+}
