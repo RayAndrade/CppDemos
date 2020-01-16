af3d6ae246472c23797ab28cb48e97d251d79e3d
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-16 10:28:35 -0800

    C++Tutorial for Beginners 28 - Headers and Prototype

---
+++ prototypes/src/prototypes.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : prototypes.cpp
+// Author      : Ray Andrade
+// Version     :
+// Copyright   : Your copyright notice
+// Description : C++Tutorial for Beginners 28 - Headers and Prototype
+//============================================================================
+
+#include <iostream>
+#include "utils.h";
+
+using namespace std;
+
+int main() {
+	doSomething();
+	doSomething();
+	return 0;
+}
+
+void doSomething() {
+	cout << "hello" << endl;
+}
---
+++ prototypes/src/utils.h
@@ -0,0 +1,13 @@
+/*
+ * utils.h
+ *
+ *  Created on: Jan 16, 2020
+ *      Author: ray
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void doSomething();
+
+#endif /* UTILS_H_ */
