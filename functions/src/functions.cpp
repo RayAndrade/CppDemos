129b432b0840d8e9fa3625a6cee27b397d233656
RayAndrade <TheRayCode@RayAndrade.com>
2020-01-16 09:53:31 -0800

    C++ for Complete Beginners 27 - Function Parameters: Passing Data to Subroutines

--- functions/src/functions.cpp
+++ functions/src/functions.cpp
@@ -5,6 +5,8 @@
 // Copyright   : Your copyright notice
 // Description : C++Tutorial for Beginners 25 - Functions: Using Subroutines in C++
 //               C++Tutorial for Beginners 26 - Return Values: Getting Data From Subroutines
+//             : C++ for Complete Beginners 27 - Function Parameters: Passing Data to Subroutines
+
 //============================================================================
 
 #include <iostream>
@@ -16,32 +18,36 @@ void showMenu() {
 	cout << "Do 3 kina stuff." << endl;
 }
 
-int processSelection() {
+int getInpupt() {
 	cout << "what kind of stuff do you want to do?" << endl;
 
 	int input1;
 	cin >> input1;
 
-	switch (input1) {
-	case 1:
-		cout << "Doing 1 kinda stuff" << endl;
-		break;
-	case 2:
-		cout << "Doing 2 kinda stuff" << endl;
-		break;
 
-	case 3:
-		cout << "Doing 3 kinda stuff" << endl;
-		break;
-	default:
-		cout << "only 1, 2 or 3" << endl;
-	}
 	return input1;
 }
+ void processSelection(int selection){
+	 switch (selection) {
+	 	case 1:
+	 		cout << "Doing 1 kinda stuff" << endl;
+	 		break;
+	 	case 2:
+	 		cout << "Doing 2 kinda stuff" << endl;
+	 		break;
+
+	 	case 3:
+	 		cout << "Doing 3 kinda stuff" << endl;
+	 		break;
+	 	default:
+	 		cout << "only 1, 2 or 3" << endl;
+	 	}
+ }
 
 int main() {
 	showMenu();
-	int input2 = processSelection();
+	int selection = getInpupt();
+	processSelection(selection);
 
 	return 0;
 }
